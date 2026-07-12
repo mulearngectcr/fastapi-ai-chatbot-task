47117c3fdae1ce472d2c883e87c3f386e0885759
maleeha07 <maleehaswalih@gmail.com>
2026-07-12 12:36:42 +0530

    fix:support both local environment varianle and cloud secrets

--- frontend.py
+++ frontend.py
@@ -1,13 +1,25 @@
+import os
 import streamlit as st
 import requests
+from dotenv import load_dotenv
 
+# Load local environment variables if they exist
+load_dotenv()
+
+# Safely look for the key without letting Streamlit crash your local app
+try:
+    app_api_key = st.secrets.get("APP_API_KEY") or os.getenv("APP_API_KEY")
+except Exception:
+    app_api_key = os.getenv("APP_API_KEY")
 st.set_page_config(page_title="AI Assistant", layout="wide")
 st.title("💬 Chatbot Studio")
 
 # Sidebar settings
 st.sidebar.header("Configuration")
-backend_url = st.sidebar.text_input("Backend URL", value="http://127.0.0.1:8000")
-secret_key = st.sidebar.text_input("Security Token", value="super_secret_handshake_token", type="password")
+backend_url = "https://ai-chatbot-zrnj.onrender.com"
+secret_key = os.getenv("APP_API_KEY") or st.secrets.get("APP_API_KEY")
+#backend_url = st.sidebar.text_input("Backend URL", value="http://127.0.0.1:8000")
+#secret_key = st.sidebar.text_input("Security Token", app_api_key = st.secrets["APP_API_KEY"], type="password")
 model = st.sidebar.selectbox("LLM Model", ["llama-3.1-8b-instant", "llama-3.3-70b-versatile", "mixtral-8x7b-32768"])
 system_prompt = st.sidebar.text_area("System Prompt", "You are a friendly AI who explains complex ideas beautifully.")
 
